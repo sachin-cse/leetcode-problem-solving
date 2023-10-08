e827ba4815cdd8c9068b909177fe583656ba00cc
sachin <mandalsachin725@gmail.com>
2023-10-08 19:55:35 +0530

    []

---
+++ without_repeating_char.py
@@ -0,0 +1,15 @@
+def longeststring(s):
+    chr = {}
+    max_length = 0
+    start = 0
+    
+    for i in range(len(s)):
+        if s[i] in chr and chr[s[i]] >= start:
+            start = chr[s[i]]
+        chr[s[i]] = i
+        max_length = max(max_length, i-start+1)
+        
+    return max_length
+
+print(longeststring('abcabcbb'))
+    
